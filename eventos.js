cb3198d7c3bfb4c4146e7d373761f9fcd199fb81
LuceroCorona <98858665+LuceroCorona@users.noreply.github.com>
2022-06-09 23:44:56 -0300

    Agregando eventos

---
+++ eventos.js
@@ -0,0 +1,4 @@
+let botonAuto = document.getElementById('btnAuto');
+let botonCamion = document.getElementById('btnCamion');
+
+botonAuto.addEventListener('click', ()=>{document.createElement('section')});
